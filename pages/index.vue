7a07f712ee99e33f92fafe45b6d5e016d42cfdc6
nymphaea-v1 <68126063+nymphaea-v1@users.noreply.github.com>
2023-02-22 23:58:07 +0500

    add sections & margin to them in index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,24 @@
 <template>
-  <c-banner />
-  <c-about />
+  <div class="content">
+    <c-banner class="content__section" />
+    <c-about class="content__section" />
+  </div>
 </template>
+
+<style scoped lang="scss">
+.content__section:not(:last-child) {
+  margin-bottom: 100px;
+}
+
+@media (min-width: $break-table-min) and (max-width: $break-table-max) {
+  .content__section:not(:last-child) {
+    margin-bottom: 80px;
+  }
+}
+
+@media (max-width: $break-phone-max) {
+  .content__section:not(:last-child) {
+    margin-bottom: 50px;
+  }
+}
+</style>
