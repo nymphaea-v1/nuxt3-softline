75ac827838458a77d597ae570096ce304ae4f2c0
nymphaea-v1 <68126063+nymphaea-v1@users.noreply.github.com>
2023-02-22 23:22:04 +0500

    add title & description to the site

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Softline',
+      meta: [
+        { name: 'description', content: 'Softline – ведущий поставщик IT-решений и сервисов.' }
+      ]
+    }
+  },
   css: ['@/assets/css/main.scss'],
   vite: {
     css: {
