66e837ca53b7a321f0b93db372091bc94fa0f12b
nymphaea-v1 <68126063+nymphaea-v1@users.noreply.github.com>
2023-02-22 20:53:35 +0500

    add button component

---
+++ components/c-button.vue
@@ -0,0 +1,38 @@
+<template>
+  <button class="button">
+    <slot />
+  </button>
+</template>
+
+<style scoped lang="scss">
+.button {
+  @extend %button-reset;
+
+  padding: 18px 28px;
+  border-radius: 8px;
+
+  color: #ffffff;
+  font-weight: 600;
+  line-height: 20px;
+
+  background-color: $primary;
+
+  transition: all 0.2s ease;
+
+  &:hover {
+    background-color: $primary-soft;
+  }
+
+  &:active {
+    filter: brightness(90%);
+  }
+
+  &:disabled {
+    color: #696969;
+
+    background-color: #b2b2b2;
+
+    pointer-events: none;
+  }
+}
+</style>
